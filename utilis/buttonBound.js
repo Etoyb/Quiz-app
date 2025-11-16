78ef5134760e602386323e8d374db6aed9a2d90e
YBCbizz <mtasacharles560@gmail.com>
2025-11-16 00:10:38 +0000

    state save

---
+++ utilis/buttonBound.js
@@ -0,0 +1,6 @@
+import { userChecker } from "../Data/userModule";
+import { isButtonAllowed } from "../Data/userModule";
+
+export function run() { console.log(isButtonAllowed)
+console.log(userChecker())
+}
